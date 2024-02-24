f8586f1f9604990a023f7de7ca336b1ceda3a4e7
Sergey_Shein <toxicmoos6.com>
2024-02-24 22:10:27 +0100

    first commit

---
+++ src/test/java/SelectMenuTests.java
@@ -0,0 +1,41 @@
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SelectMenuPage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.tests.TestBase;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SelectMenuTests extends TestBase {
+    HomePage homePage;
+    SidePanel sidePanel;
+
+    SelectMenuPage selectMenuPage;
+
+    @BeforeMethod
+    public void precondition() {
+
+        homePage = new HomePage(driver);
+        sidePanel = new SidePanel(driver);
+        selectMenuPage = new SelectMenuPage(driver);
+
+        homePage.conformCookies().getWidgets();
+        sidePanel.getSelectMenu();
+    }
+
+    @Test
+    public void selectOldStyleTest() {
+        selectMenuPage.selectOldStyle("Black");
+    }
+
+    @Test
+    public void multiSelectTest() {
+        selectMenuPage.multiSelect(new String[]{"Red","Blue","Green"});
+    }
+
+    @Test
+    public void standardMultiSelectTest() {
+        selectMenuPage.standardMultiSelect(2);
+    }
+
+}
